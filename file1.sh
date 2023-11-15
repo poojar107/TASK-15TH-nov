0a6b75eab61a6dbe40f3ca7f4b7a4d628f149e14
poojar107 <poojar1ga18cs107@gmail.com>
2023-11-15 11:08:55 +0000

    Edited in branch

---
+++ file1.sh
@@ -0,0 +1,13 @@
+echo "Enter Marks"
+read marks
+if [ $marks -gt 90 ]; then
+        echo "Distinction"
+    elif [ $marks -ge 75 ] && [ $marks -le 90 ]; then
+        echo "First class"
+    elif [ $marks -ge 60 ] && [ $marks -lt 75 ]; then
+        echo "Second class"
+    elif [ $marks -ge 35 ] && [ $marks -lt 60 ]; then
+        echo "Pass"
+    else
+        echo "Fail "
+fi
